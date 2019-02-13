3725f6443dfe2336e15eb0f80083f1824368a1d0
zhuzh <zhuzh@jiguang.cn>
2019-02-14 00:24:53 +0800

    feature: protobuf-demo

---
+++ src/main/java/com/zzh/protobuf/demo/ProtobufDemoApplication.java
@@ -0,0 +1,14 @@
+package com.zzh.protobuf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufDemoApplication.class, args);
+    }
+
+}
+
